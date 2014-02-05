e2c2901875b26e253c3a8dd6f6740d457cf54650
vagrant <vagrant@precise32.(none)>
2014-02-05 22:52:39 +0000

    added modal, and carousel, list

--- public/js/index.js
+++ public/js/index.js
@@ -9,5 +9,7 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// add any functionality and listeners you want here
+	// enable wallet click
+	$("#wallet").click(function(){$("#walletModal").modal("show");})
 }
+
