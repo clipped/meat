51d7222bdddf412d5699162667b8fcb4eb2b2f3a
vagrant <vagrant@precise32.(none)>
2014-02-15 23:26:36 +0000

    Created name sort, and started date sort

--- routes/coupons.js
+++ routes/coupons.js
@@ -4,14 +4,23 @@ exports.view = function(req, res){
 	var sort = req.query["sort"];
 
 // coupons is th elist of all the coupons
-	var coupons = [data.popularCoupons + data.availableCoupons];
-	console.log(data);
+	var coupons = data.popularCoupons.concat(data.availableCoupons);
 
 	if(sort == "Expiration Date") {
 		coupons.sort(function(a,b) {
 			var da = new Date(a.expiration),
 				db = new Date(b.expiration);
-			return a < b;
+			    console.log(da.getDay());
+			    return 1;
+		});
+	}
+	else if(sort == "Name"){
+		coupons.sort(function(a,b) {
+			if( a.name.toLowerCase() < b.name.toLowerCase())
+				return -1;
+			if( a.name.toLowerCase() > b.name.toLowerCase() )
+				return 1;
+			return 0;
 		});
 	}
 	
