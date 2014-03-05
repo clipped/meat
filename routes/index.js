d7179e9ef627403027380b6eb285e3c16e562c7d
dxu429 <dxu429@gmail.com>
2014-03-05 04:41:21 +0000

    removed dropdown coupon sort

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,9 @@
 var data =  require('../data.json');
 
 exports.view = function(req, res){
-	console.log(req.query);
-	data.buttons = false;
 	res.render('index', data);
 };
 
 exports.welcome = function(req, res) {
 	res.render('welcome');
-};
-exports.buttons = function(req, res) {
-	data.buttons = true;
-	res.render('index', data)
 };
